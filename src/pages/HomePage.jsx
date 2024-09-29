a76fc876518a97b85af34a8802e0fba767cc4a17
veljkoMiljkovic123 <mobilityworkout@gmail.com>
2024-09-29 12:41:03 +0200

    Small corect

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <div>
+      <h1 className="text-2xl uppercase font-bold text-white">
+        Go to register page 
+      </h1>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
